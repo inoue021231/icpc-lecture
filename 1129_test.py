cea6031c50636385db67b4dd5b89803e50e717a9
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-05-15 02:41:23 +0900

    1129AC

--- 1129_test.py
+++ 1129_test.py
@@ -5,5 +5,7 @@ while True:
   cards = sorted([i + 1 for i in range(n)], reverse=True)
   for i in range(r):
     p, c = map(int, input().split())
-  print(cards)
-  
+    buff = cards[p-1:p-1+c]
+    del cards[p-1:p-1+c]
+    cards = buff + cards
+  print(cards[0])
